0b5a230f0a3b08eb04b14754a55ac335bec16527
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:28:53 +0300

    1

---
+++ Params/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(5, 5);
+        }
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+            public static void Stringfunction()
+        {
+            Console.WriteLine("enter string");
+            string functionstring = Console.ReadLine();
+            Console.WriteLine(functionstring);
+        }
+
+        
+            
+    }
+}
---
+++ Week6Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HelloUser();
+            Num();
+        }
+        public static void HelloUser()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}");
+        }
+        public static void Num()
+        {
+            Console.WriteLine("enter number 1");
+            int Num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("enter number 2");
+            int Num2 = Int32.Parse(Console.ReadLine());
+            int sum = Num1 + Num2;
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
